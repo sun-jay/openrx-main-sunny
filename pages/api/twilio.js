825a3873d42a39623c3ee53bccd3be12a0b5dd5b
sun-jay <sunny.jyrm@gmail.com>
2023-02-19 15:44:48 -0800

    changed twilio

--- pages/api/twilio.js
+++ pages/api/twilio.js
@@ -6,21 +6,32 @@ export default async function handler(req, res) {
   const client = twilio(accountSid, token);
   // const { phone, message } = req.body
   // console.log(req.body);
-  client.messages
-    .create({
-      body: "Hello from OpenRx! Remember to take your " + "medicine" + " today!",
-      from: "+18443950008",
-      to: "+19255484242",
-    })
-    .then((message) =>
-      res.json({
-        success: true,
-      })
-    )
-    .catch((error) => {
-      console.log(error);
-      res.json({
-        success: false,
-      });
-    });
+
+  console.log("inside api", req.body);
+
+  // client.messages
+  //   .create({
+  //     body: "Hello from OpenRx! Remember to take your " + "medicine" + " today!",
+  //     from: "+18443950008",
+  //     to: "+19255484242",
+  //   })
+  //   .then((message) =>
+  //     res.json({
+  //       success: true,
+  //     })
+  //   )
+  //   .catch((error) => {
+  //     console.log(error);
+  //     res.json({
+  //       success: false,
+  //     });
+  //   });
+
+  // return a respose that confirms the message was sent
+  res.json({
+    success: true,
+  });
+
+
+
 }
